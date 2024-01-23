3b70a1f5c530b31cd7e5bb70bb1413ad15cff5d8
Martin <martin@marshtrio.co.uk>
2024-01-23 16:07:51 +0000

    WiFi info changed in example

--- example_net_config.h
+++ example_net_config.h
@@ -1,11 +1,14 @@
-// Enter your wifi ssid and password
+// Enter your WiFi ssid and password for network A when available
 
 const char * ssid = "***********";
 const char * password = "***********";
 
 // Retries the above password a number of times before trying the second password
-// If seconnd password also fails after retries starts again with the first
+// If second password also fails after retries starts again with the first
+// IF first succeeds A is shown on top left of display else B
 #define RETRY_PASSWORD 4;
+
+// Enter your WiFi ssid and password for alternative B network  
 
 const char * ssid2 = "***********";
 const char * password2 = "***********";
